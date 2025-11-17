cc0839dd40e248fa955d1e1f89f5865395b0379a
ahmedbrm <146023002+ahmedbrm@users.noreply.github.com>
2025-11-17 14:28:28 +0100

    update with 'origin/main'.

--- Eventify/settings.py
+++ Eventify/settings.py
@@ -47,7 +47,6 @@ INSTALLED_APPS = [
     "django.contrib.staticfiles",
     'EventApp.apps.EventappConfig',
     'rest_framework',
-    'django_filters',
     
     'EventApp.apps',
     "corsheaders",
