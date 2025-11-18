2acc21c251089aecdbdcf8b64a4b383de129a0c0
ahmedbrm <146023002+ahmedbrm@users.noreply.github.com>
2025-11-18 05:55:56 +0100

    access

--- Eventify/settings.py
+++ Eventify/settings.py
@@ -29,7 +29,14 @@ SECRET_KEY = config('SECRET_KEY' ,cast=str, default=get_random_secret_key())
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', default=False, cast=bool)
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    ".railway.app",
+    "*"
+]
+
+CSRF_TRUSTED_ORIGINS = [
+    "http://*.railway.app",
+]
 
 
 # Application definition
