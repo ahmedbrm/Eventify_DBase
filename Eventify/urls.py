d13b619623bc379b24c04555abb467aa6b216701
ahmedbrm <146023002+ahmedbrm@users.noreply.github.com>
2025-11-18 03:38:18 +0100

    app/

--- Eventify/urls.py
+++ Eventify/urls.py
@@ -2,7 +2,8 @@ from django.contrib import admin
 from django.urls import path , include
 
 urlpatterns = [
-    path("admin/", admin.site.urls),
+    path('admin/', include('admin_honeypot.urls', namespace='admin_honeypot')),
+    path("BIGBOSS/", admin.site.urls),
     path( '' , include('EventApp.urls')),
     
 ]
